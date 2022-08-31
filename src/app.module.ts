b13947f444c6014f211973f4d7c9d2e13a5861cf
George Khondar <george-eclipse@yandex.ru>
2022-08-31 14:17:28 +0300

    ConfigModule, Database

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,16 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { MongooseModule } from '@nestjs/mongoose';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { UsersModule } from './users/users.module';
 
 @Module({
-	imports: [],
+	imports: [
+		ConfigModule.forRoot(),
+		MongooseModule.forRoot(process.env.MONGO_URL),
+		UsersModule,
+	],
 	controllers: [AppController],
 	providers: [AppService],
 })
